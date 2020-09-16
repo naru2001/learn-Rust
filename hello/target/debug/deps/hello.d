61820a3bf5d29af9ae9d052945679a235628a54a
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-09-16 23:49:11 +0900

    fj

--- hello/target/debug/deps/hello.d
+++ hello/target/debug/deps/hello.d
@@ -1,5 +1,5 @@
-d:\learn-Rust\hello\target\debug\deps\hello.exe: src\main.rs
+D:\learn-Rust\hello\target\debug\deps\hello.exe: src\main.rs
 
-d:\learn-Rust\hello\target\debug\deps\hello.d: src\main.rs
+D:\learn-Rust\hello\target\debug\deps\hello.d: src\main.rs
 
 src\main.rs:
